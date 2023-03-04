5a31ec6bcf0d4a16c75d37e1a6d776046d548f94
Caleb Sullivan <CalebSullivan357@gmail.com>
2023-03-04 15:46:09 -0600

    add implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,25 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentNumber, setCurrentNumber] = useState(0)
+  let targetNumber = 5 
+
+  function add3(){
+    let result = currentNumber + 3;
+    setCurrentNumber(result);
+  }
+
 
   return (
-    <p>Numbers Game</p>
+    <div>
+      <p>Numbers Game</p>
+      <p>Target Number {targetNumber}</p>
+      <p>Current Number {currentNumber}</p>
+      <button onClick={add3}>Add 3 </button>
+      <button>Subtract</button>
+      <button>Mult by 5</button>
+
+    </div>
   )
 }
 
