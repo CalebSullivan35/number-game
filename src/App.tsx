1c9ab54c513619fc02fdfc9f5124e4100a3887bd
Caleb Sullivan <CalebSullivan357@gmail.com>
2023-03-04 16:21:13 -0600

    updated game

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
+import React from 'react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+let targetNumber = createTargetNumber();
 function App() {
-  const [currentNumber, setCurrentNumber] = useState(0)
-  let targetNumber = 5 
+  const [currentNumber, setCurrentNumber] = useState(0);
 
   function add3(){
     let result = currentNumber + 3;
     setCurrentNumber(result);
   }
 
+  function double(){
+    let result = currentNumber * 2;
+    setCurrentNumber(result);
+  }
+
+  function subtract(){
+    let result = currentNumber - 2;
+    setCurrentNumber(result);
+  }
+
+  function reset(){
+    let result = 0;
+    setCurrentNumber(result);
+  }
+
+  if (currentNumber === targetNumber){
+    return(<div>
+      <p>You Win!</p>
+      <button onClick={reset}>Reset Game!</button>
+    </div>)
+  }
 
   return (
     <div>
       <p>Numbers Game</p>
-      <p>Target Number {targetNumber}</p>
-      <p>Current Number {currentNumber}</p>
+      <p>Target Number: {targetNumber}</p>
+      <p>Current Number: {currentNumber}</p>
       <button onClick={add3}>Add 3 </button>
-      <button>Subtract</button>
-      <button>Mult by 5</button>
-
+      <button onClick={subtract}>Subtract 2</button>
+      <button onClick={double}>Double</button>
+      <button onClick={reset}>reset</button>
     </div>
   )
 }
+
+function createTargetNumber(){
+  return Math.round(Math.random() * 10)
+  
+}
+
 
 export default App
